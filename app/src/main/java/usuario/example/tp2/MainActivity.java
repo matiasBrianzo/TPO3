af32ac7eba1b6fb2c6d88d61dbfa32f1ac2997ec
MatiasBArtware <matiasb@awlatam.com>
2023-08-31 09:57:29 -0300

    fix

--- app/src/main/java/usuario/example/tp2/MainActivity.java
+++ app/src/main/java/usuario/example/tp2/MainActivity.java
@@ -1,6 +1,7 @@
 package usuario.example.tp2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -20,10 +21,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mv = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainActivityViewModel.class);
+        mv.getDatosInmueble().observe(this, new Observer<ArrayList<Inmueble>>() {
+            @Override
+            public void onChanged(ArrayList<Inmueble> inmuebles) {
+                ArrayAdapter<Inmueble> adapter = new ListaAdapter(MainActivity.this,R.layout.item,inmuebles,getLayoutInflater());
+                ListView lv=findViewById(R.id.lvmilista);
+                lv.setAdapter(adapter);
+            }
+        });
+        mv.SetDatos();
 
-        ArrayAdapter<Inmueble> adapter = new ListaAdapter(this,R.layout.item,mv.GetDatos(),getLayoutInflater());
-        ListView lv=findViewById(R.id.lvmilista);
-        lv.setAdapter(adapter);
     }
 
 }
--- app/src/main/java/usuario/example/tp2/MainActivityViewModel.java
+++ app/src/main/java/usuario/example/tp2/MainActivityViewModel.java
@@ -1,9 +1,12 @@
 package usuario.example.tp2;
 
 import android.app.Application;
+import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import java.util.ArrayList;
 
@@ -11,19 +14,32 @@ import usuario.example.tp2.modelo.Inmueble;
 
 public class MainActivityViewModel extends AndroidViewModel {
     private ArrayList<Inmueble> list;
+    private MutableLiveData<ArrayList<Inmueble>> mut_Inmueble;
+
+    private Context context;
 
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
+        this.context=application.getApplicationContext();
     }
 
+    public LiveData<ArrayList<Inmueble>> getDatosInmueble()
+    {
+        if(mut_Inmueble==null){
 
-    public ArrayList<Inmueble> GetDatos()
+            mut_Inmueble=new MutableLiveData<ArrayList<Inmueble>>();
+        }
+        return mut_Inmueble;
+    }
+
+    public void SetDatos()
     {
         list =new ArrayList<>();
         list.add(new Inmueble(R.drawable.casa,"JK",180.000));
         list.add(new Inmueble(R.drawable.casa1,"LA TOMA",100.000));
         list.add(new Inmueble(R.drawable.casa2,"MERLO",19500.000));
 
-        return list;
+        mut_Inmueble.setValue(list);
+
     }
 }
